817b6d2332ba35345cfe6823e46f48e2b80cef03
kulynych_e <kulynychevhenyy@gmail.com>
2018-10-04 23:28:08 +0300

    - add package

---
+++ src/main/java/com/muffinsoft/alexa/skills/adventureisland/InitClass.java
@@ -0,0 +1,8 @@
+package com.muffinsoft.alexa.skills.adventureisland;
+
+public class InitClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Alexa!");
+    }
+}
