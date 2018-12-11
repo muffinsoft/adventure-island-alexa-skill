a3688859aa287ab80e3a575d7c728fed847bb415
holkwen <vlad.kryshtalov@gmx.com>
2018-12-11 21:49:22 +0200

    Fallback intent follows the usual flow

--- src/main/java/com/muffinsoft/alexa/skills/adventureisland/handlers/FallbackIntentHandler.java
+++ src/main/java/com/muffinsoft/alexa/skills/adventureisland/handlers/FallbackIntentHandler.java
@@ -2,12 +2,19 @@ package com.muffinsoft.alexa.skills.adventureisland.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.Request;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
 import com.muffinsoft.alexa.skills.adventureisland.content.PhraseManager;
+import com.muffinsoft.alexa.skills.adventureisland.game.SessionStateManager;
+import com.muffinsoft.alexa.skills.adventureisland.model.DialogItem;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
+import static com.muffinsoft.alexa.skills.adventureisland.handlers.ActionIntentHandler.assembleResponse;
 
 public class FallbackIntentHandler implements RequestHandler {
     @Override
@@ -17,11 +24,16 @@ public class FallbackIntentHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput input) {
-        String speechText = PhraseManager.getPhrase("fallback");
-        return input.getResponseBuilder()
-                .withSpeech(speechText)
-                .withSimpleCard(PhraseManager.getPhrase("welcomeCard"), speechText)
-                .withReprompt(speechText)
-                .build();
+        Request request = input.getRequestEnvelope().getRequest();
+        IntentRequest intentRequest = (IntentRequest) request;
+
+        Map<String, Slot> slots = intentRequest.getIntent().getSlots();
+        SessionStateManager stateManager = new SessionStateManager(slots, input.getAttributesManager());
+        DialogItem dialog = stateManager.nextResponse();
+        dialog.setSlotName(null);
+
+        Response response = assembleResponse(dialog);
+
+        return Optional.of(response);
     }
 }
